e3616ef95dc5c219785b0fb0dd0b1303ee1c9082
DF <DF@HDF>
2016-09-14 17:13:15 +0800

    测试完毕

---
+++ spring-mongo/src/main/java/com/seezoon/mongo/dao/ChildDao.java
@@ -0,0 +1,11 @@
+package com.seezoon.mongo.dao;
+
+import org.springframework.stereotype.Repository;
+
+import com.seezoon.mongo.entity.Child;
+
+@Repository
+public class ChildDao extends MongoBaseDao<Child>{
+
+
+}
---
+++ spring-mongo/src/main/java/com/seezoon/mongo/dao/PersonDao.java
@@ -0,0 +1,11 @@
+package com.seezoon.mongo.dao;
+
+import org.springframework.stereotype.Repository;
+
+import com.seezoon.mongo.entity.Person;
+
+@Repository
+public class PersonDao extends MongoBaseDao<Person>{
+
+
+}
